135a48866d2a7e41413824b27b1e83f9666873d7
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-21 23:24:41 +0900

    leetcode: solve "4. Median of Two Sorted Arrays"

--- leetcode/algorithms/4-median-of-two-sorted-arrays/javascript.js
+++ leetcode/algorithms/4-median-of-two-sorted-arrays/javascript.js
@@ -4,5 +4,10 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  
+  let nums = nums1.concat(nums2).sort((prev, next) => prev - next);
+  if (nums.length % 2 === 1) {
+    return nums[Math.round(nums.length / 2) - 1];
+  } else {
+    return (nums[nums.length / 2 - 1] + nums[nums.length / 2]) / 2;
+  }
 };
