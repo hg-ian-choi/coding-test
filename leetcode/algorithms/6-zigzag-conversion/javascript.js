5389ba48617ffed0e259f6284afdcc0c11f53472
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-24 23:47:44 +0900

    leetcode: temp saving "6. Zigzag Conversion"

--- leetcode/algorithms/6-zigzag-conversion/javascript.js
+++ leetcode/algorithms/6-zigzag-conversion/javascript.js
@@ -4,5 +4,19 @@
  * @return {string}
  */
 var convert = function (s, numRows) {
-  
+  // can not zigzeg
+  if (numRows === 1 || s.length < numRows) {
+    return s;
+  }
+
+  // can zigzeg
+  let sArray = new Array();
+
+  for (let i = 0; i < numRows; i++) {
+    sArray[i] = new Array();
+  }
+
+  let j = 0;
+  let zigzegDirectionChanged = false;
+  let y = parseInt(s.length / numRows + 2);
 };
